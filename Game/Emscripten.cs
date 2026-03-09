1a76612e1f5b955021b3320d29d0203b2b53ae94
Samuel Paradis <126531890+SamaelHeaven@users.noreply.github.com>
2026-03-09 11:10:35 -0400

    Initial commit

---
+++ Game/Emscripten.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Game;
+
+internal static unsafe partial class Emscripten
+{
+    public const string LibraryName = "libc";
+
+    [LibraryImport(LibraryName, EntryPoint = "emscripten_set_main_loop")]
+    public static partial void SetMainLoop(delegate* unmanaged[Cdecl]<void> func, int fps, sbyte simulateInfiniteLoop);
+}
---
+++ Game/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Raylib_cs;
+
+namespace Game;
+
+internal static unsafe class Program
+{
+    // STAThread is required if you deploy using NativeAOT on Windows - See https://github.com/raylib-cs/raylib-cs/issues/301
+    [STAThread]
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Hello World");
+
+        if (OperatingSystem.IsBrowser())
+        {
+            Emscripten.SetMainLoop(&UnmanagedFrame, 0, 1);
+        }
+        else
+        {
+            while (!Raylib.WindowShouldClose())
+            {
+                Frame();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+
+    private static void Frame()
+    {
+        Raylib.BeginDrawing();
+        {
+            Raylib.ClearBackground(Color.White);
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+        }
+        Raylib.EndDrawing();
+    }
+    
+    [UnmanagedCallersOnly(CallConvs = [typeof(CallConvCdecl)])]
+    private static void UnmanagedFrame()
+    {
+        Frame();
+    }
+}
